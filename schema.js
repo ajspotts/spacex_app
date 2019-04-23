86afe5140eea5dd6b846ad92cf166f5c1f1651f8
Alexander <ajspottswood@gmail.com>
2019-04-23 15:57:12 -0400

    rendering data from spacex api

--- schema.js
+++ schema.js
@@ -9,7 +9,7 @@ const LaunchType = new GraphQLObjectType({
     mission_name: { type: GraphQLString },
     launch_year: { type: GraphQLString },
     launch_date_local: { type: GraphQLString },
-    Launch_success: { type: GraphQLBoolean },
+    launch_success: { type: GraphQLBoolean },
     rocket: { type: RocketType }
   })
 });
